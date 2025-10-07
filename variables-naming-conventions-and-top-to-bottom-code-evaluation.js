d8a4b8cbc3abc18db72f6549edde29d597685c8b
Morthallia <k.childers0914@gmail.com>
2025-10-06 19:38:33 -0500

    renamed variables

--- variables-naming-conventions-and-top-to-bottom-code-evaluation.js
+++ variables-naming-conventions-and-top-to-bottom-code-evaluation.js
@@ -24,9 +24,9 @@ Things to reflect on:
   
 */
 
-let a = "Alice";
-let b = 5;
-let c = 20;
-let d = a + " bought " + b + " items for $" + c + ".";
+let name = "Alice";
+let quantity = 5;
+let dollarAmount = 20;
+let d = name + " bought " + quantity + " items for $" + dollarAmount + ".";
 
 console.log(d);
